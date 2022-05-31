536f7c4a6a80626c12ac759e3fce1b3cad3a888f
hnuruzada <90691865+hnuruzada@users.noreply.github.com>
2022-05-31 16:55:12 +0400

    create

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Ap103PartialView.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ap103PartialView.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
